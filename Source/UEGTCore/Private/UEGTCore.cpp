d3539cdafb343ba28518765210921061ae19d26a
Michael Crosato <michael.crosato@gmail.com>
2026-09-05 01:33:14 -0700

    Prune empty Unreal module headers and unused target imports

--- Source/UEGTCore/Private/UEGTCore.cpp
+++ Source/UEGTCore/Private/UEGTCore.cpp
@@ -1,7 +1,5 @@
 // Copyright 2026 UEGT contributors. MIT License.
 
-#include "UEGTCore.h"
-
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_MODULE(FDefaultModuleImpl, UEGTCore)
--- Source/UEGTGame/Private/UEGTGame.cpp
+++ Source/UEGTGame/Private/UEGTGame.cpp
@@ -1,7 +1,5 @@
 // Copyright 2026 UEGT contributors. MIT License.
 
-#include "UEGTGame.h"
-
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, UEGTGame, "UEGTGame")
